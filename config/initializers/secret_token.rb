b1338a17b48ce03a2e7ac36a01fac153db4ff5e3
pethl <admin@fencedweller.com>
2013-05-29 16:18:18 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Town::Application.config.secret_token = '63b43db92562fd588fa14cef4eb8521cc7783d1f6cec7d3c3d70292262f6c424e7c3b20fc14281b9489724830949e3122d176b370f851f65923efc03429d8909'
